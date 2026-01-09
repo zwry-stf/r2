565534682610d6b0a98f00c652193ff98d81daad
zwry-stf <jakobyl.steffens@gmail.com>
2026-01-09 15:13:27 +0100

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -22,44 +22,8 @@ workspace "r2"
         
     location ("out/" .. action .. "/" .. host)
     
-    flags { "MultiProcessorCompile" }
-    warnings "Extra"
-    fatalwarnings { "All" }
-
-    filter "action:vs*"
-        buildoptions { "/sdl" }
-    filter {}
-    
-    filter "configurations:Debug_*"
-        symbols "On"
-        defines { "_DEBUG" }
-    filter "configurations:Release_*"
-        optimize "On"
-        intrinsics "On"
-        linktimeoptimization "On"
-        defines { "NDEBUG" }
-
-        filter "action:vs*"
-            buildoptions { "/Gy" }
-        filter {}
-    filter {}
-    
-    -- platform
-    filter "system:windows"
-        systemversion "latest"
-        defines { "R2_PLATFORM_WINDOWS" }
-    filter { "system:windows", "platforms:x86" }
-        defines { "R2_PLATFORM_WINDOWS_X86" }
-    filter { "system:windows", "platforms:x64" }
-        defines { "R2_PLATFORM_WINDOWS_X64" }
-    filter {}
-    
-    -- backend
-    filter { "configurations:*_d3d11" }
-        defines { "R2_BACKEND_D3D11" }
-    filter { "configurations:*_opengl" }
-        defines { "R2_BACKEND_OPENGL" }
-    filter {}
+dofile("premake5_common.lua")
+r2_define_common();
     
 dofile("premake5_projects.lua")
 r2_define_projects(nil, build_root, int_root)
