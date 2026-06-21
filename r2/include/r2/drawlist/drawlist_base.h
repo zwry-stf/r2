b3d316d59218413fd92af881f7206bfb2dd32880
zwry-stf <jakobyl.steffens@gmail.com>
2026-06-21 04:59:17 +0200

    added renderer access

--- r2/include/r2/drawlist/drawlist_base.h
+++ r2/include/r2/drawlist/drawlist_base.h
@@ -106,6 +106,9 @@ public:
     [[nodiscard]] std::uint32_t vertex_ptr() const noexcept {
         return static_cast<std::uint32_t>(vertices_.size());
     }
+    [[nodiscard]] renderer* get_renderer() const noexcept {
+        return renderer_;
+    }
 
 protected:
     template <typename O>
